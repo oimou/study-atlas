6b07367063143b7157b1367b2673d2a5d8778226
oimou <s1190013@gmail.com>
2021-03-17 00:10:46 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+    chainWebpack: config => {
+        config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = '定理マップ（自分用）'
+            return args
+        })
+    },
     publicPath: process.env.NODE_ENV === 'production'
         ? '/study-atlas/'
         : '/'
